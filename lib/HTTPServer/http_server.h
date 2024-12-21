30d0aea96c02088e27eccf3dd05a8e16fbed2c20
Lucas D. Franchi <LucasD.Franchi@gmail.com>
2024-12-21 18:36:17 -0300

    doc: add missing doc on http header

--- lib/HTTPServer/http_server.h
+++ lib/HTTPServer/http_server.h
@@ -4,6 +4,19 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 
+/**
+ * @file http_server.h
+ * @brief HTTP server interface for handling web requests on the ESP32.
+ */
+
+/**
+ * @brief Main execution function for the HTTP server.
+ *
+ * This function initializes and starts the HTTP server, enabling the ESP32 to
+ * handle incoming web requests. It processes requests in a FreeRTOS task.
+ *
+ * @param[in] pvParameters Pointer to task parameters (TaskHandle_t).
+ */
 void http_server_execute(void *pvParameters);
 
 #endif /* HTTP_SERVER_H */
